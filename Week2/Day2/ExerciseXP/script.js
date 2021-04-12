29fb4ccb6423c0f4f1aea8971de077398d131eaa
Fay Blash <fayblash@gmail.com>
2021-04-12 12:00:38 +0300

    first commit

---
+++ Week2/Day2/ExerciseXP/script.js
@@ -0,0 +1,90 @@
+//Exercise 1
+let x = 15;
+let y = 21;
+
+if (x > y) {
+console.log("x is the bigger number");
+}
+else if  (x < y) {
+    console.log("y is the bigger number");
+}
+else{
+    console.log("x and y are equal");
+}
+
+//Exercise 2
+let newDog = "Chihuahua";
+
+//check and display length
+console.log(newDog.length);
+
+//convert and print upp and lower
+console.log(newDog.toUpperCase());
+console.log(newDog.toLowerCase());
+
+//if else statment
+if (newDog === "Chihuahua"){
+    console.log("I love Chihuahuas, it's my favorite dog breed.");
+}
+else{
+    console.log("I don't care, I prefer cats.");
+}
+
+//Exercise 3
+let num = +prompt("Please enter a number: ");
+
+if (num%2 == 0){
+    console.log(`${num} is an even number.`)
+}
+else{
+    console.log(`${num} is an odd number`)
+}
+
+//Exercise 4
+// Using this function, answer the questions below:
+
+// function moveCommand(direction) {
+//     var whatHappens;
+//     switch (direction) {
+//         case "forward":
+//             break;
+//             whatHappens = "you encounter a monster";
+//         case "back":
+//             whatHappens = "you arrived home";
+//             break;
+//             break;
+//         case "right":
+//             return whatHappens = "you found a river";
+//             break;
+//         case "left":
+//             break;
+//             whatHappens = "you run into a troll";
+//             break;
+//         default:
+//             whatHappens = "please enter a valid direction";
+//     }
+//     return whatHappens;
+// }
+// What is the returned value when moveCommand("forward") "you encounter a monster"
+
+// What is the returned value when moveCommand("back") "you arrived home"
+
+// What is the returned value when moveCommand("right") "you found a river"
+
+// What is the returned value when moveCommand("left") "you run into a troll"
+
+//Exercise 5
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+
+if (users.length === 0){
+    console.log ("no one is online");
+}
+else if (users.length === 1){
+    console.log (`${users[0]} is online`);
+}
+else if (users.length === 2){
+    console.log (`${users[0]} and ${users[1]} are online`);
+}
+else {
+    console.log (`${users[0]} and ${users[1]} and ${users.length - 2} more are online`);
+}
---
+++ Week2/Day2/Practice/script.js
@@ -0,0 +1,20 @@
+let user = {
+    username: "FayBlash",
+    password: "******",
+}
+let database = [user];
+
+let newsfeed = [
+    {
+        "username": "asdf",
+        "timeline": "qwerty",
+    },
+    {
+        "username": "zxcv",
+        "timeline": "yuioo",
+    },
+    {
+        "username": "ghjk",
+        "timeline": "vbnm",
+    }
+]
