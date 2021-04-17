0710f5de361516da1710c9d2d4a44f2682bda621
Fay Blash <fayblash@gmail.com>
2021-04-17 23:36:24 +0300

    first commit

---
+++ Week2/Day5/DailyChallenge/script.js
@@ -0,0 +1,14 @@
+let startNum = +prompt("Enter a number from 1-99 to start the song:");
+let dropNum = startNum
+for (i=1; i<=dropNum; i++){
+console.log(`${dropNum} bottles of beer on the wall`);
+console.log(`${dropNum} bottles of beer`);
+    if(i===1){
+        console.log(`Take ${i} down, pass it around`);
+    }
+    else{
+        console.log(`Take ${i} down, pass them around`);   
+    }
+dropNum-=i;
+console.log(`${dropNum} bottles of beer on the wall`);
+}
